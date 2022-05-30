56ba18922ea1ccfb027b2d60d30dfd8ecee040cd
kgaurav113 <kgaurav501.com>
2022-05-30 15:46:20 +0530

    final

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,59 @@
 import React from "react";
-
+import Addproduct from "../components/AddProduct";
+import Pagination from "../components/Pagination";
+import { Flex,Grid } from "@chakra-ui/react";
+import axios from "axios"
+import { useEffect ,useState} from "react";
 const Products = () => {
+
+  const[page,setpage]=useState(1)
+  const[lastpage,setlastpage]=useState(0)
+  const[product,setproduct]=useState([])
+  useEffect(()=>{
+    const getdata=async()=>{
+      let r=await axios.get(`http://localhost:8080/products?_page=${page}&_limit=5`);
+      console.log(r.data)
+      setproduct(r.data)
+    }
+    getdata()
+  },[page])
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => < />;
+  // const Grid = () => <div />;
 
   return (
     <Flex>
       {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      
+      <Addproduct/>
+     {/* List of Products */}
+
+      {product.map((post)=>{
+      return(
+        <Grid>
+        <div>
+          <div className="box">
+                   <img src={post.imageSrc} alt=""/>
+          </div>
+           <p>{post.title}</p>
+          <p>{post.category}</p>
+          <p>{post.gender}</p>
+          <p>{post.price}</p>
+         
+    
+        </div>
+           
+      </Grid>
+       
+      )
+      })}
+      <br/>
+      <Pagination/>
       {/* Pagination */}
+     
     </Flex>
+  
+      
   );
 };
 
